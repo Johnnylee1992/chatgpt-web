00eee0aad70b3556f0cd02abfb65edb54d37ff49
19920205lq75321 <liqiang9205@126.com>
2023-04-27 14:19:41 +0800

    edit

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,16 @@
+<!--
+ * @Author: 19920205lq75321. liqiang9205@126.com
+ * @Date: 2023-04-27 12:33:36
+ * @LastEditors: 19920205lq75321. liqiang9205@126.com
+ * @LastEditTime: 2023-04-27 14:12:38
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+-->
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <a href="https://github.com/Johnnylee1992/chatgpt-web" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: 19920205lq75321. liqiang9205@126.com
+ * @Date: 2023-04-27 12:33:36
+ * @LastEditors: 19920205lq75321. liqiang9205@126.com
+ * @LastEditTime: 2023-04-27 14:14:40
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'userStorage'
@@ -16,8 +25,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'Johnnylee',
+      description: 'Star on <a href="https://github.com/Johnnylee1992/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
